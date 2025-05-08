f2e20c63118e0320ca7f86ded78b42fb1197977c
George Kauan Santos de Medeiros <20221101110001@ifrn.local>
2025-05-08 13:39:16 -0300

    primeira aula

---
+++ projteste/src/Componente.jsx
@@ -0,0 +1,16 @@
+function Componente() {
+    const nome = "Jogi"
+
+    return(
+        <>
+        <div className="nome">
+            <h1>Hello World!!</h1>
+        </div>
+        <div>
+            <p>Olá <a href="https://instagram.com/georgekmedeiros">{nome}</a>, você é muito bonito</p>
+        </div>
+        </>
+    )
+}
+
+export default Componente
